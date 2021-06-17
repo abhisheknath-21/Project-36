c48c0cad892c9bc55ec496cf5175e0aa1255ae73
Abhishek Nath <77669745+abhisheknath-21@users.noreply.github.com>
2021-06-17 04:08:24 -0700

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,44 @@
+class Food{
+    constructor(){
+        var foodStock;
+        var lastFed;
+
+        this.image = loadImage("images/Milk.png");
+    }
+
+    getFoodStock(){
+        var foodStock = database.ref('Food');
+        foodStock.on("value",(data)=>{
+          foodS = data.val();
+        })
+      }
+
+      updateFoodStock(foodStock){
+        database.ref('/').update({
+          Food : foodStock
+        });
+      }
+
+      
+    display(){
+        var x=80, y=100;
+
+        imageMode(CENTER);
+        image(this.image, 720, 220, 70, 70);
+
+        if(this.foodStock!=0){
+            
+            for(var i=0; i<this.foodStock; i++){
+                if(i%10==0){
+                    x=80;
+                    y=y+50;
+                }
+                image(this.image, x, y, 50, 50);
+                x=x+30;
+            }
+        }
+
+    }
+
+
+}
